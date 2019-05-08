705961d598d7d37c555457fd010c840e242fe75c
eneim <mr.nguyenhoainam@gmail.com>
2019-05-08 17:05:15 +0900

    minor renaming

--- kohii-sample/src/main/java/kohii/v1/sample/ui/echo/VideoItemHolder.kt
+++ kohii-sample/src/main/java/kohii/v1/sample/ui/echo/VideoItemHolder.kt
@@ -46,7 +46,7 @@ class VideoItemHolder(
   @SuppressLint("SetTextI18n")
   override fun onVolumeChanged(volumeInfo: VolumeInfo) {
     volumeButton.text = "Mute: ${volumeInfo.mute}"
-    viewModel.updateState(this.adapterPosition, volumeInfo)
+    viewModel.saveVolumeInfo(this.adapterPosition, volumeInfo)
   }
 
   val videoTitle = itemView.findViewById(R.id.videoTitle) as TextView
@@ -86,10 +86,10 @@ class VideoItemHolder(
           }
           .bind(playerContainer) { playback ->
             playback.addVolumeChangeListener(this@VideoItemHolder)
-            if (viewModel.volumeState.contains(this.adapterPosition)) {
-              playback.volumeInfo = viewModel.volumeState.get(this.adapterPosition)
+            if (viewModel.volumeInfoStore.contains(this.adapterPosition)) {
+              playback.volumeInfo = viewModel.volumeInfoStore.get(this.adapterPosition)
             } else {
-              viewModel.updateState(this.adapterPosition, playback.volumeInfo) // save first.
+              viewModel.saveVolumeInfo(this.adapterPosition, playback.volumeInfo) // save first.
             }
             volumeButton.text = "Mute: ${playback.volumeInfo.mute}"
             this@VideoItemHolder.playback = playback
