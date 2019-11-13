fd4cd45be26b9061062de1b7ad71a38c39e9d3f0
eneim <mr.nguyenhoainam@gmail.com>
2019-11-13 20:37:06 +0900

    Various improvement, remove generic from Playback. Add special Playback implementations.

---
+++ kohii/src/main/java/kohii/core/DynamicFragmentRendererPlayback.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2019 Nam Nguyen, nam@ene.im
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kohii.core
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.contains
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commitNow
+
+internal class DynamicFragmentRendererPlayback(
+  manager: Manager,
+  host: Host<*>,
+  config: Config,
+  container: ViewGroup
+) : Playback(manager, host, config, container) {
+
+  private val fragmentManager: FragmentManager =
+    when {
+      manager.host is Fragment -> manager.host.childFragmentManager
+      manager.host is FragmentActivity -> manager.host.supportFragmentManager
+      else -> throw IllegalArgumentException("Need ${manager.host} to have a FragmentManager")
+    }
+
+  override fun onPlay() {
+    super.onPlay()
+    rendererSetter?.shouldRequestRenderer(this)
+  }
+
+  override fun onPause() {
+    super.onPause()
+    rendererSetter?.shouldReleaseRenderer(this)
+  }
+
+  override fun <RENDERER : Any> attachRenderer(renderer: RENDERER?) {
+    if (renderer == null) return
+    require(renderer is Fragment)
+    require(container.id != View.NO_ID)
+    val existing = fragmentManager.findFragmentById(container.id)
+    if (existing !== renderer) {
+      fragmentManager.commitNow(allowStateLoss = true) { replace(container.id, renderer) }
+    } else {
+      val view = renderer.view
+      if (view != null && !container.contains(view)) {
+        val parent = view.parent
+        if (parent is ViewGroup) parent.removeView(view)
+        container.removeAllViews()
+        container.addView(view)
+      }
+    }
+  }
+
+  override fun <RENDERER : Any> detachRenderer(renderer: RENDERER?): Boolean {
+    if (renderer == null) return false
+    require(renderer is Fragment)
+    require(container.id != View.NO_ID)
+    fragmentManager.commitNow(allowStateLoss = true) { remove(renderer) }
+    return true
+  }
+}
---
+++ kohii/src/main/java/kohii/core/PlayerViewPlayable.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2019 Nam Nguyen, nam@ene.im
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kohii.core
+
+import com.google.android.exoplayer2.ControlDispatcher
+import com.google.android.exoplayer2.ui.PlayerView
+import kohii.media.Media
+import kohii.v1.Bridge
+
+internal class PlayerViewPlayable(
+  master: Master,
+  media: Media,
+  config: Config,
+  bridge: Bridge<PlayerView>
+) : Playable<PlayerView>(master, media, config, PlayerView::class.java, bridge) {
+
+  override fun shouldRequestRenderer(playback: Playback) {
+    super.shouldRequestRenderer(playback)
+    val renderer = bridge.playerView
+    if (renderer != null && playback.config.controller is ControlDispatcher) {
+      renderer.setControlDispatcher(playback.config.controller)
+      renderer.useController = true
+    }
+  }
+
+  override fun shouldReleaseRenderer(playback: Playback) {
+    val renderer = bridge.playerView
+    if (renderer is PlayerView && playback.config.controller is ControlDispatcher) {
+      renderer.setControlDispatcher(null)
+      renderer.useController = false
+    }
+    super.shouldReleaseRenderer(playback)
+  }
+}
