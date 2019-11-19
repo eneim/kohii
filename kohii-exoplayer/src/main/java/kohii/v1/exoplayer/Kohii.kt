5e1f3c8274805a01f995c9480c5082e7e1ec9ba4
eneim <mr.nguyenhoainam@gmail.com>
2019-11-19 12:44:15 +0900

    Init Kohii for ExoPlayer module

---
+++ kohii-exoplayer/src/main/java/kohii/v1/exoplayer/Kohii.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2019 Nam Nguyen, nam@ene.im
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kohii.v1.exoplayer
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import com.google.android.exoplayer2.ui.PlayerView
+import kohii.v1.ExoPlayer
+import kohii.v1.core.Creator
+import kohii.v1.core.Engine
+import kohii.v1.core.Master
+import kohii.v1.exoplayer.internal.PlayerViewPlayableCreator
+
+@ExoPlayer
+class Kohii private constructor(
+  master: Master,
+  creator: Creator
+) : Engine<PlayerView>(master, creator) {
+
+  companion object {
+
+    @Volatile private var kohii: Kohii? = null
+
+    @JvmStatic
+    operator fun get(context: Context): Kohii = kohii ?: synchronized(Kohii::javaClass) {
+      kohii ?: with(Master[context]) {
+        Kohii(this, PlayerViewPlayableCreator(this.app)).also {
+          registerEngine(it)
+          kohii = it
+        }
+      }
+    }
+
+    @JvmStatic
+    operator fun get(fragment: Fragment) = get(fragment.requireContext())
+  }
+}
