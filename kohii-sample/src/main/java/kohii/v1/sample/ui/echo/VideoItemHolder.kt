22850ad4ac41b05296c317f07ccae8cffef1f391
eneim <mr.nguyenhoainam@gmail.com>
2019-12-08 22:52:17 +0900

    Minor fix

--- kohii-sample/src/main/java/kohii/v1/sample/ui/echo/VideoItemHolder.kt
+++ kohii-sample/src/main/java/kohii/v1/sample/ui/echo/VideoItemHolder.kt
@@ -23,6 +23,7 @@ import android.widget.TextView
 import androidx.core.view.isVisible
 import com.google.android.material.button.MaterialButton
 import kohii.v1.core.Playback
+import kohii.v1.core.Playback.ArtworkHintListener
 import kohii.v1.core.Scope
 import kohii.v1.exoplayer.Kohii
 import kohii.v1.media.VolumeInfo
@@ -37,7 +38,7 @@ import kotlin.properties.Delegates
 class VideoItemHolder(
   parent: ViewGroup,
   private val kohii: Kohii
-) : BaseViewHolder(parent, R.layout.holder_video_text_overlay) {
+) : BaseViewHolder(parent, R.layout.holder_video_text_overlay), ArtworkHintListener {
 
   val videoTitle = itemView.findViewById(R.id.videoTitle) as TextView
   val videoInfo = itemView.findViewById(R.id.videoInfo) as TextView
@@ -52,58 +53,56 @@ class VideoItemHolder(
 
   internal var playback: Playback? = null
 
-  private var rawData: Video? by Delegates.observable<Video?>(null,
-      onChange = { _, _, newVal ->
-        if (newVal != null) {
-          val playlist = newVal.playlist.first()
-          this.videoItem = VideoItem(
-              newVal.title,
-              newVal.description,
-              playlist.image,
-              playlist.sources.first().file
-          )
-        } else {
-          this.videoItem = null
-        }
-      })
+  private var videoData by Delegates.observable<Video?>(null) { _, _, newVal ->
+    if (newVal != null) {
+      val playlist = newVal.playlist.first()
+      this.videoItem = VideoItem(
+          newVal.title,
+          newVal.description,
+          playlist.image,
+          playlist.sources.first().file
+      )
+    } else {
+      this.videoItem = null
+    }
+  }
 
-  private var videoItem by Delegates.observable<VideoItem?>(null,
-      onChange = { _, oldVal, newVal ->
-        if (newVal == oldVal) return@observable
-        if (newVal != null) {
-          videoTitle.text = newVal.title
-          videoInfo.text = newVal.description
-          GlideApp.with(itemView)
-              .load(newVal.imageUrl)
-              .into(videoImage)
+  private var videoItem by Delegates.observable<VideoItem?>(null) { _, oldVal, newVal ->
+    if (newVal == oldVal) return@observable
+    if (newVal != null) {
+      videoTitle.text = newVal.title
+      videoInfo.text = newVal.description
+      GlideApp.with(itemView)
+          .load(newVal.imageUrl)
+          .into(videoImage)
 
-          kohii.setUp(newVal.file) {
-            tag = requireNotNull(tagKey)
+      kohii.setUp(newVal.file) {
+        tag = requireNotNull(videoTag)
+        artworkHintListener = this@VideoItemHolder
+      }
+          .bind(playerViewContainer) { playback ->
+            this@VideoItemHolder.playback = playback
+            volumeInfo?.let { kohii.applyVolumeInfo(it, playback, Scope.PLAYBACK) }
           }
-              .bind(playerViewContainer) { playback ->
-                this@VideoItemHolder.playback = playback
-                volumeInfo?.let { kohii.applyVolumeInfo(it, playback, Scope.PLAYBACK) }
-              }
-        } else {
-          this.playback = null
-        }
-      })
+    } else {
+      this.playback = null
+    }
+  }
 
-  private val tagKey: String?
-    get() = this.videoItem?.let { "${javaClass.canonicalName}::${it.file}::$adapterPosition" }
+  private var volumeInfo by Delegates.observable<VolumeInfo?>(null) { _, _, to ->
+    if (to != null) {
+      playback?.let {
+        kohii.applyVolumeInfo(to, it, Scope.PLAYBACK)
+      }
+      volumeButton.text = "Mute: ${to.mute}"
+    }
+  }
 
-  private var volumeInfo by Delegates.observable<VolumeInfo?>(null,
-      onChange = { _, _, to ->
-        if (to != null) {
-          playback?.let {
-            kohii.applyVolumeInfo(to, it, Scope.PLAYBACK)
-          }
-          volumeButton.text = "Mute: ${to.mute}"
-        }
-      })
+  private val videoTag: String?
+    get() = this.videoItem?.let { "${it.file}::$adapterPosition" }
 
   internal fun applyVideoData(video: Video?) {
-    this.rawData = video
+    this.videoData = video
   }
 
   internal fun applyVolumeInfo(volumeInfo: VolumeInfo) {
@@ -111,7 +110,15 @@ class VideoItemHolder(
   }
 
   override fun onRecycled(success: Boolean) {
-    this.rawData = null
+    this.videoData = null
     this.volumeInfo = null
+  }
+
+  override fun onArtworkHint(
+    shouldShow: Boolean,
+    position: Long,
+    state: Int
+  ) {
+    videoImage.isVisible = shouldShow
   }
 }
