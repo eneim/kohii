96bfe46afb739302403fdbf4922d47ba12481e07
eneim <mr.nguyenhoainam@gmail.com>
2019-12-19 18:37:53 +0900

    fix docs scripts

--- release_docs.sh
+++ release_docs.sh
@@ -10,9 +10,9 @@ rm -rf docs/api
   :kohii-androidx:dokka \
   :kohii-ads:dokka
 
-cp -R art docs/art
+cp -R art docs/
 cp CHANGELOG.md docs/changelog.md
 cp CODE_OF_CONDUCT.md docs/code_of_conduct.md
 
 # Deploy to Github pages.
-mkdocs gh-deploy && rm -rf site/ && rm docs/changelog.md docs/code_of_conduct.md
+mkdocs gh-deploy && rm -rf site/ && rm docs/changelog.md docs/code_of_conduct.md && rm -R docs/art
--- serve_docs.sh
+++ serve_docs.sh
@@ -10,14 +10,9 @@ rm -rf docs/api
   :kohii-androidx:dokka \
   :kohii-ads:dokka
 
-# sed -e '/full documentation here/ { N; d; }' <README.md >docs/index.md
-cp -R art docs/art
-# cp CONTRIBUTING.md docs/contributing.md
+cp -R art docs/
 cp CHANGELOG.md docs/changelog.md
 cp CODE_OF_CONDUCT.md docs/code_of_conduct.md
-# cp coil-gif/README.md docs/gifs.md
-# cp coil-svg/README.md docs/svgs.md
-# cp logo.svg docs/logo.svg
 
 # Serve docs for local preview
 mkdocs serve
