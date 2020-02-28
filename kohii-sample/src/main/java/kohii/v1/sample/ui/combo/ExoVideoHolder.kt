b63fbb2ce90040e1824ecaef3f3c8a735edcff90
eneim <1776230+eneim@users.noreply.github.com>
2020-02-28 21:36:58 +0900

    Refactor

--- kohii-sample/src/main/java/kohii/v1/sample/ui/combo/ExoVideoHolder.kt
+++ kohii-sample/src/main/java/kohii/v1/sample/ui/combo/ExoVideoHolder.kt
@@ -30,7 +30,8 @@ import kohii.v1.sample.common.BaseViewHolder
 
 class ExoVideoHolder(
   parent: ViewGroup
-) : BaseViewHolder(parent, R.layout.holder_player_container_with_title), Playback.StateListener, ArtworkHintListener {
+) : BaseViewHolder(parent, R.layout.holder_player_container_with_title), Playback.StateListener,
+    ArtworkHintListener {
 
   internal val container = itemView.findViewById(R.id.playerContainer) as AspectRatioFrameLayout
   internal val videoTitle = itemView.findViewById(R.id.videoTitle) as TextView
