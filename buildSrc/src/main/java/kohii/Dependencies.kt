dd8b98485265c0b63660bb763f109f6b90f67d64
eneim <1776230+eneim@users.noreply.github.com>
2020-04-04 18:41:07 +0900

    Kotlin 1.3.71

--- buildSrc/src/main/java/kohii/Dependencies.kt
+++ buildSrc/src/main/java/kohii/Dependencies.kt
@@ -83,14 +83,14 @@ object Libs {
   }
 
   object Kotlin {
-    private const val version = "1.3.61"
+    private const val version = "1.3.71"
 
     const val stdlibJdk7 = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$version"
     const val stdlibJdk8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
     const val reflect = "org.jetbrains.kotlin:kotlin-reflect:$version"
     const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
     const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
-    const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3"
+    const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5"
   }
 
   object AndroidX {
