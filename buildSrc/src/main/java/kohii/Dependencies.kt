20d22c4e8b2271ea10fd526ac9cb8d74887dbf31
eneim <1776230+eneim@users.noreply.github.com>
2020-07-11 11:41:34 +0900

    ExoPlayer 2.11.7

--- buildSrc/src/main/java/kohii/Dependencies.kt
+++ buildSrc/src/main/java/kohii/Dependencies.kt
@@ -19,8 +19,8 @@ package kohii
 @Suppress("unused", "MemberVisibilityCanBePrivate")
 object Versions {
   const val exoPlayerSnapShot = "dev-v2-SNAPSHOT"
-  const val exoPlayer = "2.11.3"
-  const val exoPlayerCode = 2011003
+  const val exoPlayer = "2.11.7"
+  const val exoPlayerCode = 2011007
 }
 
 object ReleaseInfo {
