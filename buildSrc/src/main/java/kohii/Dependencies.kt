e936e2c8e7043cdf5a0ef589972226ebab3d5c99
eneim <1776230+eneim@users.noreply.github.com>
2021-01-29 22:51:42 +0900

    RC to Stable release.

--- buildSrc/src/main/java/kohii/Dependencies.kt
+++ buildSrc/src/main/java/kohii/Dependencies.kt
@@ -62,7 +62,7 @@ object BuildConfig {
   }
 
   val releaseVersionCode = gitCommitCount()
-  val releaseVersionName = "1.2.0.${Versions.exoPlayerCode}-RC2"
+  val releaseVersionName = "1.2.0.${Versions.exoPlayerCode}"
 }
 
 @Suppress("MayBeConstant", "unused")
